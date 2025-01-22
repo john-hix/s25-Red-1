3859d5a2f1be8f8fee7907acab469a8a6364f90f
John Hicks <jhick046@odu.edu>
2025-01-22 08:43:25 -0500

    [RFR] Format

--- src/common/server_config.py
+++ src/common/server_config.py
@@ -1,12 +1,16 @@
 """ Common config for the entire CueCode server application"""
+
 import os
 
-VERSION = '0.0.0'
+VERSION = "0.0.0"
+
 
 # Flask
 # pylint: disable-next=too-few-public-methods
+class FlaskConfig:
     """Handles getting config from environment variables to pass to the Flask
     application"""
-    SECRET_KEY = os.getenv('SECRET_KEY')
-    SERVER_NAME  = os.getenv('SERVER_NAME')
-    PREFERRED_URL_SCHEME  = os.getenv('PREFERRED_URL_SCHEME', 'http')
+
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    SERVER_NAME = os.getenv("SERVER_NAME")
+    PREFERRED_URL_SCHEME = os.getenv("PREFERRED_URL_SCHEME", "http")
