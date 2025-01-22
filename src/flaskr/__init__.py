d5400f665bcd615a9761dfa59951e4e81d38dc72
John Hicks <jhick046@odu.edu>
2025-01-22 08:16:34 -0500

    [RFR/FTR] Remove boilerplate and use new env var config

--- src/flaskr/__init__.py
+++ src/flaskr/__init__.py
@@ -5,30 +5,13 @@ import os
 
 from flask import Flask
 
+from common.server_config import FlaskConfig
 
-def create_app(test_config=None):
+def create_app():
     """create and configure the app"""
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY="dev",  # TODO: get config values like this from environment
-        # variables in a structured way.
-        DATABASE=os.path.join(
-            app.instance_path, "flaskr.sqlite"
-        ),  # TODO: update to Postgres
-    )
+    app = Flask(__name__)
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile("config.py", silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+    app.config.from_object(FlaskConfig())
 
     # TODO: replace with a health check endpoint
     # a simple page that says hello
