31fb39ce6792f19501b5b868e204e79eb97552f7
John Hicks <jhick046@odu.edu>
2025-01-22 08:15:52 -0500

    Add Flask config class

--- src/common/server_config.py
+++ src/common/server_config.py
@@ -4,6 +4,9 @@ import os
 VERSION = '0.0.0'
 
 # Flask
-SECRET_KEY = os.getenv('SECRET_KEY')
-SERVER_NAME  = os.getenv('SERVER_NAME')
-PREFERRED_URL_SCHEME  = os.getenv('PREFERRED_URL_SCHEME', 'http')
+class FlaskConfig():
+    """Handles getting config from environment variables to pass to the Flask
+    application"""
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SERVER_NAME  = os.getenv('SERVER_NAME')
+    PREFERRED_URL_SCHEME  = os.getenv('PREFERRED_URL_SCHEME', 'http')
