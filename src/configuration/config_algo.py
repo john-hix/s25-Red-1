cabf6cdca07b614b8bf3bcd8aa0fd389bf329811
John Hicks <jhick046@odu.edu>
2025-02-08 21:22:21 -0500

    [WIP] Write CueCode config algo from design doc in comments
    Note that the algo description disregards code organization: we
    will split things into different functions, not putting all the logic
    into `config_algo_openapi()`.

--- src/configuration/config_algo.py
+++ src/configuration/config_algo.py
@@ -16,6 +16,51 @@ def config_algo_openapi(db_engine: DBEngine, openapi_spec_id: str):
     db_engine = db_engine_for_workaround
     session = db_engine.get_session()
 
+    # Fetch OpenAPI spec from PostgreSQL
     openapi_spec = session.get(OpenAPISpec, openapi_spec_id)
+
+    # Initialize empty list of openapi_server, openapi_entity, open_api
+
+    # Encode each OpenAPI server from YAML as an openapi_server database entity
+
+    # parallel over Schema Object in OpenAPI spec
+        # parallel over HTTP verb in Path OpenAPI spec object    
+            # Initialize new OpenApiEntity object
+            
+            # Schema object has x-cuecode-prompt? Yes then
+                # Save x-cuecode-prompt to OpenApiEntity .nounPrompt
+            # else
+                # Save Schema Object name to OpenApiEntity .nounPrompt
+            
+            # Call Ollama for embedding of nounPrompt
+
+    # parallel over Schema Object in OpenAPI spec
+
+    # UPSERT entity list to PostgreSQL (but do not commit transactinon)
+
+    # Parallel over Path Object in OpenAPI spec
+        # Iterate over HTTP verb in Path object
+            # Initialize new OpenApiPath object
+            # Path object has x-cuecode-prompt? Yes then
+                # Save x-cuecode-prompt to OpenApiPath .selectionPrompt
+            # else
+                # Save Schema Object name to OpenApiPath .selectionPrompt
+            # Call Ollama for embedding of selectionPrompt
+
+    # UPSERT endpoint list to PostgreSQL (but do not commit transactinon)
+
+    # Build graph of entity relationships based on $ref pointers in OpenAPI spec
+
+    # UPSERT graph edges to PostgreSQL as openapi_entity_dependency rows
+
+    # Encode OpenAPI Paths and verbs as LLM Tool calls with the .selectionPrompt chosen earlier
+
+    # Build LLM tool call JSON from Path objects and to PostgreSQL
+
+    # Update status of config job in DB (user-facing record, not Dramatiq job)
+
+    # Commit PostgreSQL transaction
+
+    # Ack config job queue task
 
     print(openapi_spec)
