d26648cee535083ae00a9271b06df21c12dd2005
John Hicks <jhick046@odu.edu>
2025-02-25 22:58:49 -0500

    [FIX] Adjust unit tests to use SECRET_KEY as needed by WTForms

--- src/tests/test_app/test_app.py
+++ src/tests/test_app/test_app.py
@@ -15,11 +15,7 @@ from app import create_app  # type: ignore
 def app():
     """Create the entire Flask application"""
     flask_app = create_app()
-    flask_app.config.update(
-        {
-            "TESTING": True,
-        }
-    )
+    flask_app.config.update({"TESTING": True, "SECRET_KEY": "neededForTheForms"})
 
     # other setup can go here,
     # such as setting up Postgres
